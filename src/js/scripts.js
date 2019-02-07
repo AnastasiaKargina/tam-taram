5a3a76b20455378b1fc411c84bde2d2e1d0e1c90
Anastasia Kargina <kargina-98@inbox.ru>
2019-02-07 23:06:59 +0300

    Change js to upload images

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,30 +1,33 @@
-function loading() {
-    const  elem = document.getElementsByClassName('slider');
-    console.log(elem);
-    elem.style.overflow = 'auto';
-    elem.style.height = 'auto';
-    document.getElementsByClassName('loadMore').style.display = 'none';}
-    $(function () {
-        $('.slider').slick({
-            infinite: false,
-            arrows: true,
-            nextArrow: '<div class="section__slider__button-next"><img src="img/ico_dropdown.svg"width="25px"></div>',
-            prevArrow: '<div class="section__slider__button-prev"><img src="img/ico_dropdown.svg"width="25px"></div>',
-            speed: 300,
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            dots: false,
-            responsive:
-                [
-                    {
-                        breakpoint: 992,
-                        settings: {
-                            slidesToShow: 2,
-                            slidesToScroll: 2,
-                        }
-                        },
-                    {
-                        breakpoint: 768,
-                        settings: 'unslick'
-                    }        ]
-        })});
+$('.loadMore').on('click', function () {
+    this.style.display = 'none';
+    const parent = $(this).parent();
+    const videos = parent.children()[1];
+    videos.style.height = 'auto';
+    videos.style.overflow = 'auto';
+});
+$(function () {
+    $('.videos').slick({
+        infinite: false,
+        arrows: true,
+        nextArrow: '<div class="section__slider__button-next"><img src="img/ico_dropdown.svg"width="25px"></div>',
+        prevArrow: '<div class="section__slider__button-prev"><img src="img/ico_dropdown.svg"width="25px"></div>',
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: false,
+        responsive:
+            [
+                {
+                    breakpoint: 992,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: 'unslick'
+                }
+            ]
+    });
+});
